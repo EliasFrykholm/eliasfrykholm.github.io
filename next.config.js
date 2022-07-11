a48480293b7ad49e9e617bc09fc3b801a9e28fe7
Elias Frykholm <elias.frykholm@cygni.se>
2022-07-11 16:46:55 +0200

    Setup github pages deployment

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+    reactStrictMode: true,
+    swcMinify: true,
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/my-resume/' : '',
+    experimental: {
+        images: {
+            unoptimized: true,
+        },
+    },
 }
 
 module.exports = nextConfig
