51711639dd3384ecaed12f1f9af75e98bf48c130
EliasFrykholm <EliasFrykholm@users.noreply.github.com>
2022-07-16 20:42:14 +0000

    deploy: ce0733b858c031413287408da872f5da2000bede

--- _next/static/A5OONauY-5l6W93rbenWn/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/7452447bd5bb231e.css","static/chunks/pages/index-ab088a22af119190.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
---
+++ _next/static/d6LQwI6pJz9XN_3C3SlGQ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/1bfc9850-271c1e13c68c7c8a.js","static/css/f742c0bd43b82555.css","static/chunks/pages/index-cb4c423a64c172de.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
