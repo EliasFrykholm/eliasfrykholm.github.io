9ed754092e53f1429aab5fd0b4eb3a6c2c3ce3df
EliasFrykholm <EliasFrykholm@users.noreply.github.com>
2022-07-17 16:12:31 +0000

    deploy: 3a998a6adf60c32104d96d040006773d0883e86f

---
+++ _next/static/1KBzSY7T7qD53Ib7UYdoh/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/834-63e9281dcac53776.js","static/css/aa25f445d1f98729.css","static/chunks/pages/index-51f645a85eee8f84.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
--- _next/static/bkPAWiEnHzmYQirMyjg3f/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/938-42fdaa0d6e201e1b.js","static/css/aa25f445d1f98729.css","static/chunks/pages/index-1ccad32bf07a31bb.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
