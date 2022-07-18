467c01be4f909433d7f8923b03434b3e49ef6c17
Elias Frykholm <elias.frykholm@cygni.se>
2022-07-18 17:42:42 +0200

    Style changes on skill cards

--- components/SkillCard.tsx
+++ components/SkillCard.tsx
@@ -1,4 +1,5 @@
 import styles from "../styles/SkillCard.module.css";
+import { baseColorHue, baseColorSat, baseColorLight } from "../static/Constants";
 
 type Skill = {
   name: string;
@@ -11,24 +12,29 @@ type Props = {
 };
 
 function SkillCard({ skill, index }: Props) {
-  const baseColorHue = 121 + index * 10;
-  const baseColorSat = 16;
-  const baseColorLight = 56 - index * 2;
-  const color = `hsl(${baseColorHue}, ${baseColorSat}%, ${baseColorLight}%)`;
+  const hue = baseColorHue + index * 10;
+  const saturation = baseColorSat;
+  const light = baseColorLight - index * 2;
+  const color = `hsl(${hue}, ${saturation}%, ${light}%)`;
 
   return (
-    <div className={styles.skillCard} style={{ backgroundColor: color }}>
+    <div
+      className={styles.skillCard}
+      style={{
+        background: `linear-gradient(45deg, ${color} 0%, hsla(${hue + 50}, ${saturation + 40}%, ${light}%, 0.7) 100%)`,
+      }}
+    >
       <h2>{skill.name}</h2>
       <div
         className={styles.skillIndicatorContainer}
-        style={{ backgroundColor: `hsl(${baseColorHue}, ${baseColorSat - 10}%, ${baseColorLight}%)` }}
+        style={{ backgroundColor: `hsl(${hue}, ${saturation + 20}%, ${light - 30}%)` }}
       >
         <div
           className={styles.skillIndicator}
           style={{
-            background: `linear-gradient(90deg, hsl(${baseColorHue + 20}, ${baseColorSat + 30}%, ${
-              baseColorLight - 30
-            }%) 0%, hsl(${baseColorHue + 20}, ${baseColorSat + 30}%, ${baseColorLight}%) 100%)`,
+            background: `linear-gradient(90deg, hsl(${hue + 20}, ${saturation + 30}%, ${light - 30}%) 0%, hsl(${
+              hue + 20
+            }, ${saturation + 30}%, ${light}%) 100%)`,
             width: `${skill.score * 10}%`,
           }}
         >
