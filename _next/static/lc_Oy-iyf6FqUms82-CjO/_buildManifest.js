5fccdee7461c536ab2875cbb61a3c344d6c22e61
EliasFrykholm <EliasFrykholm@users.noreply.github.com>
2022-07-22 06:15:35 +0000

    deploy: 684cec1337cce2f6b1635a206ef0034a7a512ee0

--- _next/static/lc_Oy-iyf6FqUms82-CjO/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/619-03da15ee4c240d9c.js","static/css/f7ce89d32746fffa.css","static/chunks/pages/index-7bc0a24191d80ae0.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
