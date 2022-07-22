d069a4f58fa0c9c59a98400e5dd9d7e43926eb51
Elias Frykholm <elias.frykholm@cygni.se>
2022-07-22 07:48:54 +0200

    Format content

--- pages/index.tsx
+++ pages/index.tsx
@@ -75,50 +75,67 @@ const Home: NextPage = () => {
             <h2>Cygni, Part of Accenture - Fullstack Engineer Consultant</h2>
             <h3>Malmö, Sweden - September 2021 - Now</h3>
             <p>
-              Elias together with two other Cygni talents created a web application for booking rooms and desks at the
-              Cygni Syd office using mob-programming. The application uses a React Frontend with Google SSO and
-              connections to the Google Calender API as backend. A simple node.js express backend with sqlite was also
-              written during development for testing purposes. The application was then deployed using AWS Amplify.
+              Cygni, part of Accenture is a consultancy firm specialized in digital solutions. <br />
+              <br />I have been part of the Talent Program at Cygni which focuses heavily on personalized competence
+              development and targets newly graduates in parallell to being on an assignment at a client. I have had the
+              opportunity to further develop my competence in areas such as AWS, Java Spring Boot, NoSQL databases,
+              agile processes etc through this program.
             </p>
+            <h3 className={styles.waveSectionFooter}>
+              Technologies: AWS, Java, Spring Boot, Agile, React, MongoDB, .NET, Scrum
+            </h3>
           </WaveSection>
           <WaveSection scrollOffset={offsetY} coloredComponents={coloredComponents}>
             <h2>Axis Communications - Master Thesis Student</h2>
             <h3>Lund, Sweden - October 2021 - June 2021</h3>
             <p>
               Axis Communications AB is a Swedish manufacturer of network based solutions in the areas of physical
-              safety and camera surveillance. I did my master thesis in monitoring of large scale distributed software
-              systems. The goal of the thesis was to explore the possibilities of discovering different types of network
-              faults between the nodes in the systems, both within and outside kubernetes clusters using a recent
-              technology, service mesh. This involved creating a virtualized test-environment consisting of multiple
-              virtual machines running different services, connected through a simulated network where faults could be
-              introduced. A series of tests were carried out in order to evaluate if the faults could be discovered
-              using the service mesh. The result of the master thesis was that network faults could to some extent be
-              discovered using the solution and could be applicable in some cases. The problems experienced were mainly
-              due to using service mesh in a way it is not intended for.
+              safety and camera surveillance. <br />
+              <br /> I did my master thesis in monitoring of large scale distributed software systems. The goal of the
+              thesis was to explore the possibilities of discovering different types of network faults between the nodes
+              in the systems, both within and outside kubernetes clusters using a recent technology, service mesh. This
+              involved creating a virtualized test-environment consisting of multiple virtual machines running different
+              services, connected through a simulated network where faults could be introduced. A series of tests were
+              carried out in order to evaluate if the faults could be discovered using the service mesh. <br />
+              <br />
+              The result of the master thesis was that network faults could to some extent be discovered using the
+              solution and could be applicable in some cases. The problems experienced were mainly due to using service
+              mesh in a way it is not intended for.
             </p>
+            <h3 className={styles.waveSectionFooter}>
+              Technologies: Kubernetes, Service-mesh, Vagrant, Helm, Ansible, Docker, SDN, PromQL, Istio
+            </h3>
           </WaveSection>
           <WaveSection scrollOffset={offsetY} coloredComponents={coloredComponents}>
             <h2>Nibe AB - Fullstack Developer</h2>
-            <h3>Markaryd, Sweden (Remote)- June 2020 - September 2020 (Full-time)</h3>
-            <h3>Markaryd, Sweden (Remote)- November 2020 - December 2020 (Part-time)</h3>
+            <h3>
+              Markaryd, Sweden (Remote)- June 2020 - September 2020 (Full-time) <br /> Markaryd, Sweden (Remote)-
+              November 2020 - December 2020 (Part-time)
+            </h3>
             <p>
               Nibe is a Swedish manufacturer with a global presence. The company develops, manufactures and sells energy
-              solutions in the areas of real estate, industry and infrastructure. I was a summer intern at Nibe in 2020
-              which later transferred to a part-time position alongside my studies until it was time for my master
-              thesis. The work was carried out remotely and i was working on an internal tool to be used for testing of
-              the manufactured machines in which the machines could be remotely controlled. The program was developed
-              using React.js for the frontend together with Electron and backend in C# .NET Core.
+              solutions in the areas of real estate, industry and infrastructure. <br />
+              <br />I was a summer intern at Nibe in 2020 which later transferred to a part-time position alongside my
+              studies until it was time for my master thesis. The work was carried out remotely and i was working on an
+              internal tool to be used for testing of the manufactured machines in which the machines could be remotely
+              controlled. The program was developed using React.js for the frontend together with Electron and backend
+              in C# .NET Core.
             </p>
+            <h3 className={styles.waveSectionFooter}>
+              Technologies: JavaScript, TypeScript, Redux, React.js , Electron, C#, .NET Core, Git, Jira
+            </h3>
           </WaveSection>
           <WaveSection scrollOffset={offsetY} coloredComponents={coloredComponents}>
             <h2>Calmon Stepper Motor Technologies - Embedded Developer Summer Intern</h2>
             <h3>Karlshamn, Sweden - June 2019 - August 2019 (Full-time)</h3>
             <p>
               Calmon is a consulting company in the field of electrical engineering, specializing in stepper motor
-              controllers. I was a summer intern at Calmon in 2019, responsible for an internal project of which to be
-              used for demo-purposes to show the precision of their stepper motor controllers. The project involved some
+              controllers. <br />
+              <br />I was a summer intern at Calmon in 2019, responsible for an internal project of which to be used for
+              demo-purposes to show the precision of their stepper motor controllers. The project involved some
               electrical engineering and programming in C++.
             </p>
+            <h3 className={styles.waveSectionFooter}>Technologies: C++</h3>
           </WaveSection>
           <WaveSection scrollOffset={offsetY} coloredComponents={coloredComponents} isBottom>
             <h2>Lund Formula Student - Embedded Developer</h2>
@@ -128,11 +145,14 @@ const Home: NextPage = () => {
               car and compete with other teams around Europe. The Lund Formula student team was a team of 70 students
               and this year building two cars, one conbustion car and one concept electric car. Everyone was in a small
               team responsible for a certain subsystem of the car and I was responsible for the code in the custom built
-              embedded systems. The project is carried out in the timespan of a year. The Lund Formula student team is
-              one of the top-ranking teams in Europe in the combustion section, however the competitions was canceled in
-              2020 due to covid-19. Some of the students however, continued working on the car for some time and I was
-              one of them which led to the car being able to compete in 2021.
+              embedded systems. <br />
+              <br />
+              The project is carried out in the timespan of a year. The Lund Formula student team is one of the
+              top-ranking teams in Europe in the combustion section, however the competitions was canceled in 2020 due
+              to covid-19. Some of the students however, continued working on the car for some time and I was one of
+              them which led to the car being able to compete in 2021.
             </p>
+            <h3 className={styles.waveSectionFooter}>Technologies: C++, CAN</h3>
           </WaveSection>
         </section>
         <section id="education">
@@ -141,16 +161,11 @@ const Home: NextPage = () => {
             <h2>Lund University - Master of Science, Computer Science</h2>
             <h3>Lund, Sweden - August 2016 - June 2021</h3>
             <p>
-              Axis Communications AB is a Swedish manufacturer of network based solutions in the areas of physical
-              safety and camera surveillance. I did my master thesis in monitoring of large scale distributed software
-              systems. The goal of the thesis was to explore the possibilities of discovering different types of network
-              faults between the nodes in the systems, both within and outside kubernetes clusters using a recent
-              technology, service mesh. This involved creating a virtualized test-environment consisting of multiple
-              virtual machines running different services, connected through a simulated network where faults could be
-              introduced. A series of tests were carried out in order to evaluate if the faults could be discovered
-              using the service mesh. The result of the master thesis was that network faults could to some extent be
-              discovered using the solution and could be applicable in some cases. The problems experienced were mainly
-              due to using service mesh in a way it is not intended for.
+              Lund University the top ranked university in Sweden 2022, and one of two Swedish universities currently in
+              the top 100.
+              <br />
+              <br />I studied the five year engineering program Computer Science and for my master, i specialized in
+              Software Engineering.
             </p>
           </WaveSection>
           <WaveSection scrollOffset={offsetY} coloredComponents={coloredComponents}>
@@ -162,7 +177,7 @@ const Home: NextPage = () => {
               understanding of Scrum as described in the Scrum Guide. This individual has also demonstrated a consistent
               use of terminology and approach to Scrum.
             </p>
-            <h3>
+            <h3 className={styles.waveSectionFooter}>
               Verify: <a href="https://www.credly.com/badges/afa64343-1040-4c6e-bdef-c5bde0c0400c">Credly</a>
             </h3>
           </WaveSection>
@@ -174,7 +189,7 @@ const Home: NextPage = () => {
               implementing cloud initiatives. Earning AWS Certified Cloud Practitioner validates cloud fluency and
               foundational AWS knowledge.
             </p>
-            <h3>
+            <h3 className={styles.waveSectionFooter}>
               Verify: <a>Credly</a>
             </h3>
           </WaveSection>
@@ -186,7 +201,7 @@ const Home: NextPage = () => {
               cloud initiatives. Earning AWS Certified Solutions Architect - Associate validates the ability to design
               and implement distributed systems on AWS.
             </p>
-            <h3>
+            <h3 className={styles.waveSectionFooter}>
               Verify: <a>Credly</a>
             </h3>
           </WaveSection>
@@ -307,16 +322,16 @@ const Home: NextPage = () => {
               This was a personal project where i wanted to develop a mobile app for recommending movies, with the main
               purpose of trying out some new technologies that i hadnt worked with previously such as graph database,
               recommendation engines and Flutter.
-            </p>
-            <p>
+              <br />
+              <br />
               The application is built using the cross-platform framework Flutter and has only been tested on Android
               but the framework should allow it to be exported for browser and ios. The backend is built using Java with
               Spring Boot and consits of two microservices, one for all interactions towards the TMDB api and one for
               database interactions towards the Neo4J graph database and providing recommendations. The recommendation
               engine works by finding movies that other users has liked that has a history of likeing previous movies
               that the current user also has liked.
-            </p>
-            <p>
+              <br />
+              <br />
               When a user first opens the application, they are presented with a genre selection screen where they can
               pick genres that they prefer. The selected genres are mainly used for the initial recommendations but is
               also weighed in for future recommendations. After that, a movie artwork is shown based on the calculated
@@ -325,9 +340,8 @@ const Home: NextPage = () => {
               to watch the trailer. If there are no recommendations for the current user, the application fetches movies
               from the TMDB toplist and adds them to the recommendation engine for future users. There is no sign in
               required for the application, it only uses an unique id based on the device to identify the user.
-            </p>
-
-            <p>
+              <br />
+              <br />
               The application heavily relies on the open api movie database TMDB (<a>https://www.themoviedb.org/</a>)
               which serves all the movie data.
             </p>
@@ -342,14 +356,14 @@ const Home: NextPage = () => {
               This was a personal project where i developed a notes application inspired by Google Keep, with the main
               purpose of improving my skills or trying some new technologies technologies such as Microservice
               Architecture, AWS, Kubernetes, Golang, Kubernetes, CI/CD, Terraform.
-            </p>
-            <p>
+              <br />
+              <br />
               The frontend for the application is built using React and the backend consists two services, one
               authentification service (written in Golang) and one service for handling the notes (written in java) and
               a MongoDB database for storing notes and user data. The application was deployed to an AWS EKS cluster
               using Terraform and the process is fully automated with Github Actions.
-            </p>
-            <p>
+              <br />
+              <br />
               The frontend is a React application and is a traditional note taking interface built with Material UI
               displaying all the users notes, allows for editing of the notes, changing colors, token refreshing,
               searching for notes and with a login/signup component. The authentication for this application is built
@@ -372,15 +386,15 @@ const Home: NextPage = () => {
               semi-multiplayer/realtime game which tests the players coding skills. The game consists of a frontend
               built using react, a backend server in Java, and several client templates created in different popular
               languages.
-            </p>
-            <p>
+              <br />
+              <br />
               The gist of the game is that you are multiple players on a vertically autoscrolling 2D game map, the
               players should dig their way down through the ground as far as possible but there are several objects
               randomly scattered throughout the map. There are pick-up items in the map such as explosives and extra
               points and there are stones in your way that cant be dug but can be destroyed with explosives. You also
               got to watch out for other players as they can destroy you by colliding with you or by explosives.
-            </p>
-            <p>
+              <br />
+              <br />
               The game works by a server rendering the map which is served to the frontend and handling all game logic,
               there are multiple clients connected through the server (players) which performs actions for the players.
               The players objective is to download a client in their preferred coding language which has some predefined
@@ -398,9 +412,8 @@ const Home: NextPage = () => {
             <h3>Technologies: TypeScript, React, Next.js, CSS, Github Actions</h3>
             <p>
               This webpage is built using Next.js and automatically deployed to Github pages through Github Actions. The
-              components and styiling are custom built with CSS and SVG graphics, you might already have noticed but
-              there is a parallaxing effect on the background and the wavy sections move when you scroll. Colors for the
-              sections is somewhat autogenerated. The page is built to be easily editable.
+              components and styiling are custom built with CSS and SVG graphics. The page is built with a focus to have
+              easily editable content.
             </p>
           </WaveSection>
         </section>
